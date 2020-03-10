365b82948d8ea43750d6e7489b75537c8117a5a3
James Mills <james.mills@jammills-m-v1dr.home>
2020-03-10 13:19:45 +0000

    First Commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,21 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ProfileComponent } from './profile/profile.component';
+import { OrderpizzaComponent } from './orderpizza/orderpizza.component';
+import { AuthGuard } from './auth.guard';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'profile',
+    component: ProfileComponent
+  },
+  {
+    path: 'orderpizza',
+    component: OrderpizzaComponent,
+    canActivate: [AuthGuard]
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/orderpizza/orderpizza.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../auth.service';
+
+@Component({
+  selector: 'app-orderpizza',
+  templateUrl: './orderpizza.component.html',
+  styleUrls: ['./orderpizza.component.css']
+})
+export class OrderpizzaComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+    
+  }
+
+}
