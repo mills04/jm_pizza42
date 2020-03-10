963d73ad2c0da88d29bb5e3c7a6c669e459decc7
James Mills <james.mills@jammills-m-v1dr.home>
2020-03-10 19:04:22 +0000

    Auth Changes

--- src/app/auth.service.ts
+++ src/app/auth.service.ts
@@ -123,4 +123,12 @@ export class AuthService {
     });
   }
 
+  function (user, context, callback) {
+  if (!user.email_verified) {
+    return callback(new UnauthorizedError('Please verify your email before logging in.'));
+  } else {
+    return callback(null, user, context);
+  }
+}
+
 }
